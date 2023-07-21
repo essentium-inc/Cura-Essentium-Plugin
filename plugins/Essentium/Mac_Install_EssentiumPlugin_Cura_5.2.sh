9715dd823e395207b3733e28ad4552476a476d31
Alexander Yozzo <alexander.yozzo@essentium.com>
2023-07-20 22:10:31 -0500

    Adding untested MaxOS dev scripts.

---
+++ plugins/Essentium/Mac_Install_EssentiumPlugin_Cura_5.2.sh
@@ -0,0 +1,2 @@
+cp ./EssentiumPlugin/ "~Library/Application Support/cura/5.2/plugins/EssentiumPlugin/EssentiumPlugin/"
+read -p -s "Press any key to continue..."
---
+++ plugins/Essentium/Mac_Install_EssentiumPlugin_Cura_5.4.sh
@@ -0,0 +1,2 @@
+cp ./EssentiumPlugin/ "~Library/Application Support/cura/5.4/plugins/EssentiumPlugin/EssentiumPlugin/"
+read -p -s "Press any key to continue..."
